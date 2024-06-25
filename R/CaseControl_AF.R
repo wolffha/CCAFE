b96f96a6a9411b949c19ec5b4f0d1277216b6bd5
wolffha <150075196+wolffha@users.noreply.github.com>
2024-06-25 14:03:52 -0600

    Upload all files from R project

---
+++ R/CaseControl_AF.R
@@ -0,0 +1,89 @@
+#' @title CaseControl_AF
+#' @description
+#' This is a function to derive the case and control AFs from GWAS summary statistics when
+#' the user has access to the whole sample AF, the sample sizes, and the OR (or beta).
+#' If user has SE instead of sample AF use [CaseControlAF::CaseControl_SE()]
+#'
+#' @param N_case the number of cases in the sample
+#' @param N_control the number of controls in the sample
+#' @param OR a vector with the ORs
+#' @param AF_population a vector with the observed sample AFs
+#'
+#' @note
+#'  Make sure the vectors are in the same order for your variants
+#'
+#' @return returns a dataframe with two columns (AF_case, AF_control) and rows
+#' equal to the number of variants
+#'
+#' @author Hayley Wolff (Stoneman), \email{hayley.wolff@cuanschutz.edu}
+#'
+#' @references https://github.com/wolffha/CaseControlAF
+#'
+#' @seealso \url{https://github.com/wolffha/CaseControlAF} for further documentation
+#'
+#' @examples
+#' data("sampleDat")
+#'
+#' nCase_sample = 79148
+#' nControl_sample = 61106
+#'
+#' # get the estimated case and control AFs
+#' af_method_results <- CaseControl_AF(N_case = nCase_sample,
+#'                                     N_control = nControl_sample,
+#'                                     OR = sampleDat$OR,
+#'                                     AF_population = sampleDat$true_maf_pop)
+#'
+#' head(af_method_results)
+#'
+#' @import genpwr
+#' @export
+CaseControl_AF <- function(N_case, N_control, OR, AF_population){
+
+  # do input checking
+  if(length(OR) != length(AF_population)) {
+    stop("ERROR: 'OR' and 'AF_population' vectors need to be the same length")
+  }
+  if(typeof(OR) != "double") {
+    stop("ERROR: 'OR' vector needs to be a vector of numbers (hint: check for NAs)")
+  }
+  if(typeof(AF_population) != "double") {
+    stop("ERROR: 'AF_population' vector needs to be a vector of numbers (hint: check for NAs)")
+  }
+  if(N_case <= 0) {
+    stop("ERROR: 'N_case' needs to be a number > 0")
+  }
+  if(N_control <= 0) {
+    stop("ERROR: 'N_control' needs to be a number > 0")
+  }
+
+  # uses the genpwr package for the quad_roots function to solve for the roots of quadratic
+  #require(genpwr)
+
+  #calculate total sample size
+  N_total = N_control+N_case
+
+  #set a, b, c of quadratic equation derived in manuscript
+  a = (N_control/N_case)*(OR-1)
+  b = (OR-((N_total/N_case)*AF_population*OR))+((N_control/N_case)+(N_total*AF_population/N_case))
+  c = -(N_total/N_case)*AF_population
+
+  AF_control <- rep(0, length(a))
+
+  for(i in 1:length(a)) {
+    #find roots of quadratic eq and choose root between 0 and 1 as AF_control
+    AF_control_opts =  genpwr::quad_roots(a[i], b[i], c[i])
+    if(AF_control_opts[1]>1 | AF_control_opts[1]<0){
+      AF_control[i] = AF_control_opts[2]
+    }else{
+      AF_control[i] = AF_control_opts[1]
+    }
+  }
+
+
+  #calculate AF_case with known relationship shown in manuscript
+  AF_case = (N_total/N_case)*AF_population - (N_control/N_case)*AF_control
+
+  #Output shows case AF first, then control AF
+  return(data.frame(AF_case = AF_case,
+                    AF_control = AF_control))
+}
---
+++ R/CaseControl_SE.R
@@ -0,0 +1,134 @@
+#' @title CaseControl_SE
+#' @description This is a function to derive the case and control AFs from GWAS summary statistics when
+#' the user has access to the whole sample AF, the sample sizes, and the OR (or beta).
+#' If user has SE instead of sample AF use [CaseControlAF::CaseControl_SE()]
+#' This code uses the GroupFreq function adapted from C from <https://github.com/Paschou-Lab/ReAct/blob/main/GrpPRS_src/CountConstruct.c>
+#'
+#' @param N_case an integer of the number of Case individuals
+#' @param N_control an integer of the number of Control individuals
+#' @param OR a vector of odds ratios
+#' @param SE a vector of standard errors for the OR calculation *make sure the indices match between the two vectors
+#'
+#' @note
+#'  Make sure the vectors are in the same order for your variants
+#'
+#' @return a dataframe with 3 columns: MAF_case, MAF_control, MAF_pop for the estimated MAFs for each variant
+#'
+#' @author Hayley Wolff (Stoneman), \email{hayley.wolff@cuanschutz.edu}
+#'
+#' @references https://github.com/wolffha/CaseControlAF
+#'
+#' @seealso \url{https://github.com/wolffha/CaseControlAF} for further documentation
+#'
+#' @examples
+#' data("sampleDat")
+#'
+#' nCase_sample = 79148
+#' nControl_sample = 61106
+#'
+#' # get the estimated case and control MAFs
+#' se_method_results <- CaseControl_SE(N_case = nCase_sample,
+#'                                     N_control = nControl_sample,
+#'                                     OR = sampleDat$OR,
+#'                                     SE = sampleDat$SE)
+#'
+#' head(se_method_results)
+#'
+#' @import genpwr
+#' @export
+CaseControl_SE <- function(N_case, N_control, OR, SE) {
+  # genpwr package is used to solve for the roots of the quadratic equation
+  #require(genpwr)
+
+  # do input checking
+  if(length(OR) != length(SE)) {
+    stop("ERROR: 'OR' and 'SE' vectors need to be the same length")
+  }
+  if(typeof(OR) != "double") {
+    stop("ERROR: 'OR' vector needs to be a vector of numbers (hint: check for NAs)")
+  }
+  if(typeof(SE) != "double") {
+    stop("ERROR: 'SE' vector needs to be a vector of numbers (hint: check for NAs)")
+  }
+  if(N_case <= 0) {
+    stop("ERROR: 'N_case' needs to be a number > 0")
+  }
+  if(N_control <= 0) {
+    stop("ERROR: 'N_control' needs to be a number > 0")
+  }
+
+  # this function uses w, x, y, z as the derivation in the ReACt paper does - see their supplement
+  w = SE^2
+  x = 2*N_case
+  y = 2*N_control
+  z = OR
+
+  AC_control = rep(0, length(OR))
+  AC_case = rep(0, length(OR))
+
+  for(i in 1:length(OR)) {
+    # need to make sure the discriminate will be positive or it won't solve
+  #inflate w(se) by 1.001, for at max 49 times (usually can be done within 5 iterations.
+  #maximum inflation is 0.050195, ~5%), if disc still < 0 then give up
+    for(j in 0:99) {
+      w[i] = w[i] * (1.001^j)
+      disc = (((2*z[i]*y*(1-z[i])) - (w[i]*x*y*z[i]))^2) - 4*(w[i]*x*z[i] + (1-z[i])^2)*(y*z[i]*(x + y*z[i]))
+      if (!is.na(disc) & disc >= 0) {
+        break
+      }
+    }
+    if (is.na(disc) | disc < 0) { # if discriminate is <0 cannot solve and return 0s
+      AC_control[i] = 0
+      AC_case[i] = 0
+    } else { # now actually solve the quadratic for allele counts (AC)
+        # solve for the a, b, and c of the quadratic equation
+        # this quadratic is solving for the allele count (AC) of the controls
+        # overall their derivation relies on AC (rather than AF) and then calculates AF
+        a = (w[i]*x*z[i]) + (1-z[i])^2
+        b = 2*y*z[i]*(1-z[i]) - w[i]*x*y*z[i]
+        c = y*z[i]*(x + y*z[i])
+
+         #find roots of quadratic equation
+        AF_control_opts =  genpwr::quad_roots(a, b, c)
+        # in order to select which root, we need to use each option (d1 and d2) to calculate a, b, c of the 2x2 table of allele counts
+        d1 = AF_control_opts[1]
+        c1 = y - d1
+        b1 = (x*d1)/(y*z[i] - z[i]*d1 + d1)
+        a1 = x - b1
+
+        d2 = AF_control_opts[2]
+        c2 = y - d2
+        b2 = (x*d2)/(y*z[i] - z[i]*d2 + d2)
+        a2 = x - b2
+
+        vec1 = c(a1, b1, c1, d1) # vector of a,b,c,d using root 1
+        vec2 = c(a2, b2, c2, d2) # vector of a,b,c,d using root 2
+
+        # if both roots allow for all values to be positive, choose the larger
+        if(!any(vec1 < 0) & !(any(vec2 < 0))) {
+          if(d1 > d2) { # if d1 is the larger root, then the AC_control = c1 and AC_case = a1
+            AC_control[i] = c1
+            AC_case[i] = a1
+          } else {
+            AC_control[i] = c2
+            AC_case[i] = a2
+          }
+        } else if(!any(vec1 < 0)) { # if d1 allows all values of 2x2 to be positive but NOT d2, use c1 and a1
+          AC_control[i] = c1
+          AC_case[i] = a1
+        } else { # if d2 allows all values to be positive but NOT d1, use c2 and a2
+          AC_control[i] = c2
+          AC_case[i] = a2
+        }
+      }
+    }
+
+  # calculate and return the MAF
+  MAF_case = AC_case/(2*N_case)
+  MAF_control = AC_control/(2*N_control)
+  MAF_pop = (MAF_case*N_case + MAF_control*N_control)/(N_case + N_control)
+
+  return(data.frame(MAF_case = MAF_case,
+                    MAF_control = MAF_control,
+                    MAF_pop = MAF_pop))
+}
